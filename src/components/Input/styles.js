9ea1c18c1602142dd49da68d67ace97727e6e977
Raphael Dorta <raphael_dorta@hotmail.com>
2024-11-12 07:21:40 -0300

    Primeiro commit - adicionando projeto de calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    
+    font-size: 24px;
+    
+    
+    input {
+        padding: 0 10px;
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        diplay: flex;
+        flex-direction: column;
+        align-items: flex-end;
+
+        font-size: 24px;
+        color: #ffffff
+    }
+`
---
+++ src/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #CACACA;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #FAFAFA;
+    width: 50%;
+`
+
+export const Row = styled.div`
+    display: flex;]
+    justify-direction: row;
+    justify-content: space-between;
+    align-itens: center;
+`
+export const Collumn = styled.div`
+    display: flex;
+    justify-direction: collumn;
+    justify-content: space-between;
+    align-itens: center;
+`
